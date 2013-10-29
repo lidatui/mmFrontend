da975c4889da03f194294fa5e45dd81ab8bc47f3
miemiedev <dev@miemie.li>
2013-10-29 21:20:22 +0800

    init

---
+++ Gruntfile.js
@@ -0,0 +1,67 @@
+'use strict';
+
+module.exports = function (grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+    // Task configuration.
+    clean: {
+      files: ['dist']
+    },
+    concat: {
+      options: {
+        banner: '<%= banner %>',
+        stripBanners: true
+      },
+      things: {
+        src: ['**/*.js'],
+        cwd: 'src/',
+        dest: 'dest/',
+        expand: true
+      }
+    },
+    uglify: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      things: {
+        src: ['**/*.js'],
+        cwd: 'dest/',
+        dest: 'dest/',
+        expand: true,
+        ext: '.min.js'
+      }
+    },
+    jshint: {
+      gruntfile: {
+        options: {
+          jshintrc: '.jshintrc'
+        },
+        src: 'Gruntfile.js'
+      },
+      src: {
+        options: {
+          jshintrc: 'src/.jshintrc'
+        },
+        src: ['src/**/*.js']
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'clean', 'concat', 'uglify']);
+
+};
---
+++ dest/directive/lazyLoad/lazyLoad.js
@@ -0,0 +1,55 @@
+/*! mmFrontend - v0.1.0 - 2013-10-29
+* https://github.com/miemiedev/mmFrontend
+* Copyright (c) 2013 miemiedev; Licensed MIT */
+'use strict';
+
+angular.module('mm.lazyLoad',[])
+    .directive('mmLazyLoad',['$document','$window'],function($document,$window){
+        var addEvent = function(evnt, elem, func) {
+            if (elem.addEventListener){
+                elem.addEventListener(evnt,func,false);
+            }else if (elem.attachEvent) {
+                elem.attachEvent("on"+evnt, func);
+            }else {
+                elem[evnt] = func;
+            }
+        };
+
+        var getWindowSize = function(){
+            var wWidth,wHeight;
+            if( typeof( $window.innerWidth ) === 'number' ) {
+                wWidth = $window.innerWidth;
+                wHeight = $window.innerHeight;
+            } else if( $document.documentElement && ( $document.documentElement.clientWidth || $document.documentElement.clientHeight ) ) {
+                wWidth  = $document.documentElement.clientWidth;
+                wHeight = $document.documentElement.clientHeight;
+            }
+            return {
+                width: wWidth,
+                height: wHeight
+            };
+        };
+
+        var link = function(scope, element, attrs){
+            var show = false;
+            addEvent('scroll',$window,function(){
+                var wSize = getWindowSize();
+                var elTop = element[0].getBoundingClientRect().top;
+                if(elTop < wSize.height && !show){
+                    var mmLazyLoad = attrs.mmLazyLoad;
+                    if(mmLazyLoad){
+                        var tagName = element.prop('tagName');
+                        if(tagName === 'IMG'){
+                            element.prop('src',mmLazyLoad);
+                        }else{
+                            element.addClass(mmLazyLoad);
+                        }
+                    }
+                    show = true;
+                }
+            });
+        };
+        return {
+            link: link
+        };
+    });
